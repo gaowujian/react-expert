33858e840589d443a8b7c95b7870e493d73a421a
Tony Gao <806003097@qq.com>
2020-08-05 18:01:43 +0800

    测试setstate

--- src/index.js
+++ src/index.js
@@ -1,53 +1,26 @@
-import React from "./react";
-import ReactDOM from "./react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// function Welcome() {
-//   return (
-//     <div className="container" title="我是一个title" style={{ color: "red" }}>
-//       hello <span>world!</span>
-//     </div>
-//   );
-// }
-// ==>等价于
-// function Welcome() {
-//   return /*#__PURE__*/ React.createElement(
-//     "div",
-//     {
-//       className: "container",
-//       title: "\u6211\u662F\u4E00\u4E2Atitle",
-//       style: {
-//         color: "red",
-//         fontSize: "28px",
-//       },
-//     },
-//     "hello ",
-//     /*#__PURE__*/ React.createElement("span", null, "world!")
-//   );
-// }
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 1,
+    };
+  }
 
-class Welcome extends React.Component {
+  add = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
   render() {
-    return React.createElement(
-      "div",
-      {
-        className: "container",
-        title: "\u6211\u662F\u4E00\u4E2Atitle",
-        style: {
-          color: "red",
-          fontSize: "28px",
-        },
-      },
-      "hello ",
-      /*#__PURE__*/ React.createElement("span", null, "world!")
+    console.log(this);
+    return (
+      <div>
+        <p>number:{this.state.number}</p>
+        <button onClick={this.add}>+</button>
+      </div>
     );
   }
 }
 
-// <Welcome/>
-// ==>等价于
-// React.createElement(Welcome, null);
-// const element = Welcome();
-
-const element = React.createElement(Welcome, null);
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));
