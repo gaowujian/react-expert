6c059b8b45c889392023673b74f39703688f7019
Tony Gao <gwjlshebei@gmail.com>
2020-08-07 23:04:12 +0800

    实现模态框

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./modal.css";
 
-export default class App extends React.Component {
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.model = document.getElementById("model-root");
+  }
   render() {
-    return <div>app</div>;
+    return ReactDOM.createPortal(this.props.children, this.model);
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleModal}>显示/关闭</button>
+        {this.state.showModal && (
+          <Modal>
+            <div className="modal">
+              <div className="content">
+                <header>头部</header>
+                <div>内容</div>
+                <button onClick={this.toggleModal}>关闭模态框</button>
+                <footer></footer>
+              </div>
+            </div>
+          </Modal>
+        )}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
