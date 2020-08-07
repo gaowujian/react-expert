10bd782454fd73da2ff0c159dbb02e72ccd544f7
Tony Gao <gwjlshebei@gmail.com>
2020-08-07 22:33:46 +0800

    clear

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class App extends React.Component {
+  render() {
+    return <div>app</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
